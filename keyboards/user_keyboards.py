c77094a76d23a56626083ab8fdcb54d8a6a0e73c
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-05-01 23:28:33 +0300

    Создание клавиатуры "Главного меню"

--- keyboards/user_keyboards.py
+++ keyboards/user_keyboards.py
@@ -7,8 +7,10 @@ def main_menu_keyboard() -> InlineKeyboardMarkup:
         [InlineKeyboardButton(text="⭐️ Услуги и цены", callback_data="services_and_prices")],
         [InlineKeyboardButton(text='🗒 Бланк заказа', callback_data="order_form")],
         [InlineKeyboardButton(text='🛍 Самовыкуп', callback_data="self_purchase")],
-        [InlineKeyboardButton(text='📦 Виды упаковки', callback_data="types_packaging")],
-        [InlineKeyboardButton(text='💌 Отзывы', callback_data="reviews")],
+        [
+            InlineKeyboardButton(text='📦 Виды упаковки', callback_data="types_packaging"),
+            InlineKeyboardButton(text='💌 Отзывы', callback_data="reviews"),
+        ],
         [InlineKeyboardButton(text='📚 Полезная информация', callback_data="useful_information")],
         [InlineKeyboardButton(text='🔄 Обновить бота', callback_data="update_bot")],
         [InlineKeyboardButton(text='📞 Связаться с менеджером', callback_data="contact_manager")],
