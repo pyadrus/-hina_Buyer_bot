6e747103833ba94742321422fb9bcd6ce576421d
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-05-09 00:08:04 +0300

    Добавлено меню: "Услуга Поиска товаров (производителей в Китае)"

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from handlers.delivery_goods_from_china_handlers import delivery_goods_from_china_handlers_register_message_handler
 from handlers.goods_redemption_service_handlers import goods_redemption_service_handlers_register_message_handler
 from handlers.main_menu_handlers import main_menu_register_message_handler
+from handlers.product_search_service_handlers import product_search_service_handlers_register_message_handler
 from handlers.selection_goods_analyst_sale_marketplaces_handlers import \
     selection_goods_analyst_sale_marketplaces_register_message_handler
 from handlers.services_and_prices_handlers import services_and_prices_register_message_handler
@@ -19,6 +20,8 @@ async def main() -> None:
     selection_goods_analyst_sale_marketplaces_register_message_handler()
     delivery_goods_from_china_handlers_register_message_handler()
     goods_redemption_service_handlers_register_message_handler()
+    product_search_service_handlers_register_message_handler()
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
