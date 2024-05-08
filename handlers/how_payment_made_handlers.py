fe74fa35df93cbd071a14b3c52b65d2ac43a9454
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-05-09 00:17:29 +0300

    Добавлено меню: "Как совершается оплата?"

---
+++ handlers/how_payment_made_handlers.py
@@ -0,0 +1,39 @@
+import json
+
+from aiogram import types, F
+from aiogram.types import FSInputFile, InputMediaPhoto
+from loguru import logger
+
+from keyboards.user_keyboards import selection_goods_keyboard
+from system.dispatcher import bot
+from system.dispatcher import dp
+from system.dispatcher import router
+
+
+def load_bot_info_services_and_prices():
+    with open("messages/how_payment_made_messages.json", 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@router.callback_query(F.data == "payment_process")
+async def how_payment_made_handlers(callback_query: types.CallbackQuery):
+    logger.debug(callback_query)
+    logger.debug(callback_query.message.message_id)
+    main_menu_key = selection_goods_keyboard()
+
+    data = load_bot_info_services_and_prices()
+    # document = FSInputFile('messages/image/4.png')
+    # media = InputMediaPhoto(caption=data)
+
+    await bot.edit_message_caption(
+                                   chat_id=callback_query.message.chat.id,
+                                   message_id=callback_query.message.message_id,
+                                   caption=data,
+                                   reply_markup=main_menu_key
+                                   )
+
+
+def how_payment_made_handlers_register_message_handler():
+    """Регистрируем handlers для бота"""
+    dp.message.register(how_payment_made_handlers)
